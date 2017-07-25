b2b2f628bea247b437e4f6e9b3635e5a587c0633
Antonio Broughton <antoniob@attachesoftware.com>
2017-07-25 19:03:37 +1200

    refactor tests
    playing with passing in the functions being tested... into a function... since the code was basically the same...
    
    quite the neat :D

--- testcalc_test.go
+++ testcalc_test.go
@@ -6,53 +6,42 @@ import (
 	"github.com/worzeel/testcalc"
 )
 
+type calcChecks func(...int) int
+type values []struct {
+	numbers       []int
+	expectedValue int
+}
+
+func doMultipleChecks(fn calcChecks, vals values, t *testing.T) {
+	for _, theTests := range vals {
+		val := fn(theTests.numbers...)
+
+		if val != theTests.expectedValue {
+			t.Errorf("Value should be %d but was %d", theTests.expectedValue, val)
+		}
+	}
+}
+
 func TestIShouldBeAbleToCallAddToObtainKnownValues(t *testing.T) {
-	for _, theTests := range []struct {
-		numbers       []int
-		expectedValue int
-	}{
+	doMultipleChecks(testcalc.Add, values{
 		{[]int{1, 2}, 3},
 		{[]int{1, 2, 3}, 6},
 		{[]int{1, 5, 2}, 8},
-	} {
-		val := testcalc.Add(theTests.numbers...)
-
-		if val != theTests.expectedValue {
-			t.Errorf("Value should be %d but was %d", theTests.expectedValue, val)
-		}
-	}
+	}, t)
 }
 
 func TestIShouldBeAbleToCallSubtractToObtainKnownValues(t *testing.T) {
-	for _, theTests := range []struct {
-		numbers       []int
-		expectedValue int
-	}{
+	doMultipleChecks(testcalc.Subtract, values{
 		{[]int{1, 2}, -1},
 		{[]int{3, 2, 1}, 0},
 		{[]int{10, 5, 2}, 3},
-	} {
-		val := testcalc.Subtract(theTests.numbers...)
-
-		if val != theTests.expectedValue {
-			t.Errorf("Value should be %d but was %d", theTests.expectedValue, val)
-		}
-	}
+	}, t)
 }
 
 func TestIShouldBeAbleToCallMultiplyToObtainKnownValues(t *testing.T) {
-	for _, theTests := range []struct {
-		numbers       []int
-		expectedValue int
-	}{
+	doMultipleChecks(testcalc.Multiply, values{
 		{[]int{1, 2}, 2},
 		{[]int{3, 2, 1}, 6},
 		{[]int{10, 5, 2}, 100},
-	} {
-		val := testcalc.Multiply(theTests.numbers...)
-
-		if val != theTests.expectedValue {
-			t.Errorf("Value should be %d but was %d", theTests.expectedValue, val)
-		}
-	}
+	}, t)
 }
